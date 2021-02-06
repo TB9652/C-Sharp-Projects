4052c260dc3cfb535b3a165707a3021d7f2ede49
TB9652 <w.emond9652@gmail.com>
2021-02-06 13:06:56 -0800

    Added projects

---
+++ CarInsurance/CarlInsurance/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using CarlInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarlInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+    }
+}
